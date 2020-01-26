4df43671d22f107c68333fa9d7884c58bc16dc70
vitor.rigolon <vrigolon@mail.com>
2020-01-26 12:31:29 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app>
+		<app-menu />
+		<app-toolbar />
+		<v-content>
+			<app-carousel />
+		</v-content>
+		<app-footer />
+		
+	</v-app>
+</template>
+
+<script>
+import Footer from './Footer.vue'
+import Carousel from './Carousel'
+import Toolbar from './Toolbar'
+import Menu from './Menu.vue'
+
+
+
+export default {
+	components: { 'app-footer': Footer, 'app-carousel': Carousel, 'app-toolbar': Toolbar, 'app-menu':Menu },
+
+}
+</script>
